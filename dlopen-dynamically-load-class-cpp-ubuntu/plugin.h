f795fb6a2d487eee33b7cf3168874826592b4292
Pham Van Lam <lampv606@gmail.com>
2017-02-16 15:01:26 +0700

    Create plugin.h

---
+++ dlopen-dynamically-load-class-cpp-ubuntu/plugin.h
@@ -0,0 +1,18 @@
+#ifndef _PLUGIN_H_
+#define _PLUGIN_H_
+
+class Plugin
+{
+    public:
+        // default constructor
+        virtual void test() { };
+        virtual ~Plugin() {};
+};
+
+class PluginFactory
+{
+    public:
+        virtual Plugin * getInstance() { };
+};
+
+#endif
